d7d844beea980dd4deca694868d49f7ef6145a7c
jtCodes <jt234wq@gmail.com>
2019-04-29 10:51:29 -0400

    Updates

---
+++ precache-manifest.8796454c27dc1850423c281b7d7b761c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b0ce4870fa1737bfc420cb09b1bc9f",
+    "url": "/react-temp-chart/index.html"
+  },
+  {
+    "revision": "d3c95155d92ee1595153",
+    "url": "/react-temp-chart/static/css/main.fb272593.chunk.css"
+  },
+  {
+    "revision": "ca69d63727a498b80d86",
+    "url": "/react-temp-chart/static/js/2.331054f5.chunk.js"
+  },
+  {
+    "revision": "d3c95155d92ee1595153",
+    "url": "/react-temp-chart/static/js/main.9a9b86fb.chunk.js"
+  },
+  {
+    "revision": "783f0c5de3f850466f8b",
+    "url": "/react-temp-chart/static/js/runtime~main.9bd1815c.js"
+  }
+]);
